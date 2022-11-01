611322c6df3f720271a1062d237bca31f6998af7
Annie Pylypchuk <anechkaaaaaaa@gmail.com>
2022-11-01 13:41:30 +0200

    Homework - Ex2, Ex3

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 	`use strict`;
 
-	// 1. **Задача на повернення ініціалів для кожного імені з масиву, посортованих в алфавітному порядку:**
+// 2. **Задача на розворот числа:**
+
+// const currentMaxValue = 4589;
+// let reverseMaxValue;
+
+// reverseMaxValue = (Array.from(String(currentMaxValue))).reverse();
+// reverseMaxValue = reverseMaxValue.join('');
+// reverseMaxValue = +reverseMaxValue;
+
+// console.log(reverseMaxValue); // 9854
+// console.log(typeof reverseMaxValue); // 'number'
+
+// 3. **Задача на знаходження добутку масиву чисел з невідомою глибиною вкладеності:**
+
+// const resultsArray = [1, 2, [3, [4]]];
+// let productOfArray;
+
+// productOfArray = resultsArray.flat(Infinity);
+// productOfArray = productOfArray.reduce(
+// 	(a, b) => a * b);
+
+// console.log(productOfArray); // 24 -->
+
+
+
+// 1. **Задача на повернення ініціалів для кожного імені з масиву, посортованих в алфавітному порядку:**
 
 // const userNames = [ "Петрик Ольга Іванівна", 
 // 					"Гнатюк Петро Антонович", 
@@ -26,15 +51,5 @@
 
 // console.log(initials); // [ "Г.П.А", "П.О.І", "Р.А.О"]
 
-// // 1. **Задача на розворот числа:**
 
-const currentMaxValue = 4589;
-let reverseMaxValue;
-
-reverseMaxValue = (Array.from(String(currentMaxValue))).reverse();
-reverseMaxValue = reverseMaxValue.join('');
-reverseMaxValue = +reverseMaxValue;
-
-console.log(reverseMaxValue); // 9854
-console.log(typeof reverseMaxValue); // 'number'
 
