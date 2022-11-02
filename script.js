4500a200bcebd80ed9fd0bc951f376fc68d7a5ad
Annie Pylypchuk <anechkaaaaaaa@gmail.com>
2022-11-02 11:10:06 +0200

    HomeWork - 31.10 - ex3, ex 2, ex 1 - not all

--- script.js
+++ script.js
@@ -2,54 +2,49 @@
 
 // 2. **Задача на розворот числа:**
 
-// const currentMaxValue = 4589;
-// let reverseMaxValue;
+const currentMaxValue = 4589;
+let reverseMaxValue;
 
-// reverseMaxValue = (Array.from(String(currentMaxValue))).reverse();
-// reverseMaxValue = reverseMaxValue.join('');
-// reverseMaxValue = +reverseMaxValue;
+reverseMaxValue = (Array.from(String(currentMaxValue))).reverse();
+reverseMaxValue = reverseMaxValue.join('');
+reverseMaxValue = +reverseMaxValue;
 
-// console.log(reverseMaxValue); // 9854
-// console.log(typeof reverseMaxValue); // 'number'
+console.log(reverseMaxValue); // 9854
+console.log(typeof reverseMaxValue); // 'number'
 
 // 3. **Задача на знаходження добутку масиву чисел з невідомою глибиною вкладеності:**
 
-// const resultsArray = [1, 2, [3, [4]]];
-// let productOfArray;
+const resultsArray = [1, 2, [3, [4]]];
+let productOfArray;
 
-// productOfArray = resultsArray.flat(Infinity);
-// productOfArray = productOfArray.reduce(
-// 	(a, b) => a * b);
+productOfArray = resultsArray.flat(Infinity);
+productOfArray = productOfArray.reduce(
+	(a, b) => a * b);
 
-// console.log(productOfArray); // 24 -->
+console.log(productOfArray); // 24 -->
 
 
 
 // 1. **Задача на повернення ініціалів для кожного імені з масиву, посортованих в алфавітному порядку:**
 
-// const userNames = [ "Петрик Ольга Іванівна", 
-// 					"Гнатюк Петро Антонович", 
-// 					"Рудко Андрій Опанасович"];
-// let initials;
-
-// // initials = (userNames.sort()).map(
-// // 	(user) => user.charAt(0));
+const userNames = [ "Петрик Ольга Іванівна", 
+					"Гнатюк Петро Антонович", 
+					"Рудко Андрій Опанасович"];
+let initials;
 
 
 
-// initials = userNames;
+initials = userNames.sort().join(' ').split(' ').map(word => word[0]).join(".").split(" ");
 
-// // // initials = userNames.map (
-// // // 	(user) => user.charAt(0));
-
-// initials.forEach(
-// 	(user) => (user.toString()).charAt(0)
-// 	);
-	
-
-// // // console.log(userNames);
-
-// console.log(initials); // [ "Г.П.А", "П.О.І", "Р.А.О"]
+console.log(initials); // [ "Г.П.А", "П.О.І", "Р.А.О"]
 
 
 
+initials = ((userNames.sort()).map(
+	(users) => users.split(" ")));
+console.log('на підмасиви -->', initials);
+
+initials.forEach(array => console.log("виводить перші букви", array.map(([word]) => word[0]).join('.').split(" ")));
+
+
+//а як його об'єднати в одне я не знаю
